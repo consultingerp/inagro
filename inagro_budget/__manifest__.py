4cce594a34692c24c1e2a3acbf94db99bfb164af
rachmadona48 <rachmadona@rocketmail.com>
2019-04-27 15:51:48 +0700

    data laptop

--- inagro_budget/__manifest__.py
+++ inagro_budget/__manifest__.py
@@ -9,7 +9,7 @@
         -
     """,
 
-    'author': "INAGRO",
+    'author': "INAGRO tes",
     'website': "https://www.inagro.co.id/",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
