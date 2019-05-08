244fd0a6ee8b70de7048fe5ac891a3695c91d207
rachmadona48 <rachmadona@rocketmail.com>
2019-05-08 16:07:21 +0700

    fuel loq driver

--- inagro_fleet/models/inherit_FleetVehicleLogFuel.py
+++ inagro_fleet/models/inherit_FleetVehicleLogFuel.py
@@ -5,15 +5,11 @@ from odoo import models, fields, api
 class inherit_FleetVehicleLogFuel(models.Model):
     _inherit = 'fleet.vehicle.log.fuel'
 
-    purchaser_id = fields.Many2one('res.partner', 'Purchaser', domain="[('customer','=',False)]")
+    purchaser_id = fields.Many2one('res.partner', 'Purchaser')
 
 
 class inherit_FleetVehicleLogServices(models.Model):
     _inherit = 'fleet.vehicle.log.services'
 
-    purchaser_id = fields.Many2one('res.partner', 'Purchaser', domain="[('customer','=',False)]")
+    purchaser_id = fields.Many2one('res.partner', 'Purchaser')
 
-class inherit_FleetVehicleLogContract(models.Model):
-    _inherit = 'fleet.vehicle.log.contract'
-
-    notes = fields.Text('Information', help='Write here all supplementary information relative to this contract', copy=False)
