6204e16775638a157594e08db874032757859a77
rachmadona48 <rachmadona@rocketmail.com>
2019-05-11 15:36:34 +0700

    inagro diskusi marketing

---
+++ inagro_diskusi_marketing/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Inagro Diskusi Marketing",
+
+    'summary': """
+        rayci232@gmail.com""",
+
+    'description': """
+        -
+    """,
+
+    'author': "INAGRO",
+    'website': "https://www.inagro.co.id/",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['mail','base'],
+
+    # always loaded
+    'data': [
+        'security/group.xml',
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/diskusi_marketing.xml',
+        'views/room.xml',
+        'views/form_diskusi.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
