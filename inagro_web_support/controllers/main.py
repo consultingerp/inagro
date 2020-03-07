ee1ad2b5c90c4044fcd039a1bd4d5113af901338
ff4f <faliqulfikri@outlook.co.id>
2020-03-07 13:47:55 +0700

    bug fixing inagro website support

--- inagro_web_support/controllers/main.py
+++ inagro_web_support/controllers/main.py
@@ -38,6 +38,27 @@ class CustomSupportTicketController(http.Controller):
 
         return http.request.render('website_support.support_submit_ticket', {'department_id':department_categories,'categories': ticket_categories, 'priorities': http.request.env['website.support.ticket.priority'].sudo().search([]), 'person_name': person_name, 'email': http.request.env.user.email, 'setting_max_ticket_attachments': setting_max_ticket_attachments, 'setting_max_ticket_attachment_filesize': setting_max_ticket_attachment_filesize, 'setting_google_recaptcha_active': setting_google_recaptcha_active, 'setting_google_captcha_client_key': setting_google_captcha_client_key, 'setting_allow_website_priority_set': setting_allow_website_priority_set})
     
+    @http.route('/support/help', type="http", auth="public", website=True)
+    def support_help(self, **kw):
+        """Displays all help groups and thier child help pages"""
+
+        permission_list = []
+        group_support = False
+        for perm_group in request.env.user.groups_id:
+            permission_list.append(perm_group.id)
+            if perm_group.category_id.name == 'Website Support':
+                group_support = True
+                
+
+        help_groups = http.request.env['website.support.help.group'].sudo().search(['|', ('group_ids', '=', False ), ('group_ids', 'in', permission_list), ('website_published','=',True)])
+
+        setting_allow_user_signup = request.env['ir.default'].get('website.support.settings', 'allow_user_signup')
+
+        manager = False
+        if request.env['website.support.department.contact'].sudo().search_count([('user_id','=',request.env.user.id)]) == 1:
+            manager = True
+
+        return http.request.render('website_support.support_help_pages', {'help_groups': help_groups, 'setting_allow_user_signup': setting_allow_user_signup, 'manager': manager, 'group_support':group_support})
     
     @http.route('/support/subcategories/fetch_department', type='http', auth="public", website=True)
     def support_categories_fetch(self, **kwargs):
